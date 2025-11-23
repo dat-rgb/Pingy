43403b5f249dafbbe71e7bfa33616d95e0e41b14
dat-rgb <trandatc3vvk@gmail.com>
2025-11-23 20:01:20 +0700

    first commit

---
+++ frontend/vite.config.ts
@@ -0,0 +1,18 @@
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  optimizeDeps: {
+    force: true,
+    exclude: ['@radix-ui/react-label'] // tránh duplicate file
+  }
+})
