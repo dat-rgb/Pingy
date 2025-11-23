b9b3b6bf7c59df1f73c818bcf70eb38cda0354a6
dat-rgb <trandatc3vvk@gmail.com>
2025-11-23 21:51:36 +0700

    Hoàn thiện hệ thống xác thực người dùng JWT

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -134,3 +134,36 @@ export const signOut = async (req, res) => {
     return res.status(500).json({ message: "Lỗi hệ thống" });
   }
 };
+
+export const refreshToken = async(req, res) => {
+  try {
+    // Lấy refreshToken từ cookie
+    const token = req.cookies?.refreshToken;
+    if(!token) {
+      return res.status(401).json({message: "Token không tồn tại."});
+    }
+    
+    // So với refreshToken trong database
+    const session = await Session.findOne({refreshToken: token});
+    if(!session) {
+      return res.status(403).json({message: "Token không tồn tại hoặc đã hết hạn."});
+    }
+
+    // Kiểm tra hết hạn
+    if(session.expiresAt < new Date()) {
+      return res.status(403).json({message: "Token đã hết hạn."});
+    }
+
+    // Tạo accesstoken mới
+    const accessToken = jwt.sign({
+      userId: session.userId
+    }, process.env.ACCESS_TOKEN_SECRET, {expiresIn: ACCESS_TOKEN_TTL});
+
+    // return
+    return res.status(200).json({accessToken});
+
+  } catch (error) {
+    console.error("Lỗi khi gọi refreshToken", error);
+    return res.status(500).json({message: "Lỗi hệ thống"});
+  }
+};
--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -5,7 +5,11 @@ export const authMe = async (req, res) => {
         return res.status(200).json({user});
 
     } catch (error) {
-        console.error("Lỗi khi gọi autMe", error);
+        console.error("Lỗi khi gọi authMe", error);
         return res.status(500).json({message: "Lỗi hệ thống."})
     }
+}
+
+export const test = async (req, res) => {
+    return res.sendStatus(204);
 }
